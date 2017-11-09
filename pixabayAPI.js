84ba534ddc19f8709c406291fee846908592352c
Diana <dianaltecson@gmail.com>
2017-11-09 08:34:21 -0800

    added image access and random number generator

--- pixabayAPI.js
+++ pixabayAPI.js
@@ -1,8 +1,11 @@
-// pixabay API access
-var key = '6982377-68fe5b4423fc7e3f952f46c15';
-var testTerm = 'flower';
+// variable declaration
+var selectedTerm;
+// word bank -- randomly select search term from here
 var searchTerms = ['puppy','cats','pink flowers','trees and sun'];
-var queryUrl = 'https://pixabay.com/api/?key=' + key + '&q=' + testTerm + '&image_type=photo&pretty=true';
+// add an indicator to determine if a term was already searched/displayed to the users
+	// true/false asked property
+	// create a counter -- if it matches the amount of images we want to show, stop the game
+
 
 $(document).ready(function () {
 
@@ -14,13 +17,25 @@ $('button').click(function () {
 		return Math.floor(Math.random() * max) + min;
 	}
 
+	// create function that selects random word from word bank and random hit from ajax response
+		// when invoking function, be sure to store result in variable
+	function randomSelect (array) {
+		return array[generateRandomNum(0, array.length)];
+	}
+
+	// pixabay API access
+	var key = '6982377-68fe5b4423fc7e3f952f46c15';
+	var queryUrl = 'https://pixabay.com/api/?key=' + key + '&q=' + 'trees and sun' + '&image_type=photo&pretty=true';
+
 	$.ajax({
 		url: queryUrl,
 		method: 'GET'
 	}).done(function (response) {
 		// results is a list of the hits we received after running a search on selected search term
 		var results = response.hits
+		var image = $('<img>').attr('src', results[0].previewURL);
 		var tags = results[0].tags;
+		$('body').append(image);
 		console.log(response);
 		console.log(tags);
 	});
