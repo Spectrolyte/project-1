f3eddf3af6e4597aa4fdac533659cccd9b6c33f5
Your Name <enzocnop@gmail.com>
2017-11-10 13:19:15 -0800

    visual frame

---
+++ assets/javascript/firebaseAuth.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAKovIgnElTXfZog6-eGf7X3vU1I7go6yI",
+  authDomain: "imagewordmatch.firebaseapp.com",
+  databaseURL: "https://imagewordmatch.firebaseio.com",
+  projectId: "imagewordmatch",
+  storageBucket: "imagewordmatch.appspot.com",
+  messagingSenderId: "621229379920"
+};
+firebase.initializeApp(config);
