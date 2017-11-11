0c5443b09f0817fca242d20d8a628a14bfbece89
Diana <dianaltecson@gmail.com>
2017-11-11 12:03:23 -0600

    added player2 placeholder

--- pixabayAPI.js
+++ pixabayAPI.js
@@ -11,10 +11,13 @@ var searchTerms = ['puppy','cats','pink flowers','trees and sun'];
 $(document).ready(function () {
 
 	// points accumulated by user
-	var points = 0;
+	var p1points = 0;
+	var p2points = 0;
 	// stores user guesses to be referenced to later and compared
-	var guesses = [];
+	var p1guesses = [];
+	var p2guesses = [];
 
+// change click event to function on setTimeout -- each round lasts 30 seconds
 $('#generate-photo-info').click(function () {
 	//create random number generator
 		// to select random word from our word bank
@@ -67,43 +70,53 @@ $('#generate-photo-info').click(function () {
 		// iterate through tags and check to see if they exist in the guesses array
 			// refactor this code later for multiplayer feature
 		function addBonusPoints () {
-			// number of matches
-			var matches = 0;
-			// number of bonus points earned
-			var bonusPoints = 0;
 
 			for (var i=0; i < tags.length; i++) {
-
-				if (guesses.includes(tags[i])) {
-					matches++;
+				// if player one guessed a correct tag, award 2 points
+				if (p1guesses.includes(tags[i])) {
+					p1points += 2;
+				}
+				// if player one guessed a correct tag, award 2 points
+				if (p2guesses.includes(tags[i])) {
+					p2points += 2;
 				}
 			}
 
-			bonusPoints = matches * 2;
-
-			// update total points;
-			points += bonusPoints;
-
-			console.log(matches);
-			console.log(bonusPoints);
-			console.log(points);
+			console.log(p1points);
+			console.log(p2points);
 		}
 
 		// capture user input and store in array
-		$('#submit-btn').click(function (event) {
+		$('#p1-submit-btn').click(function (event) {
 			// prevent page reload
 			event.preventDefault();
 
 			// capture user input
-			var guess = $('#guess-input').val();
-			console.log(guess);
+			var p1guess = $('#p1-guess').val();
+			console.log(p1guess);
 
 			// adds user guess to guesses array
-			guesses.push(guess);
-			console.log(guesses);
+			p1guesses.push(p1guess);
+			console.log(p1guesses);
 
 			// clear input field
-			$('#guess-input').val('');
+			$('#p1-guess').val('');
+		})
+
+		$('#p2-submit-btn').click(function (event) {
+			// prevent page reload
+			event.preventDefault();
+
+			// capture user input
+			var p2guess = $('#p2-guess').val();
+			console.log(p2guess);
+
+			// adds user guess to guesses array
+			p2guesses.push(p2guess);
+			console.log(p2guesses);
+
+			// clear input field
+			$('#p2-guess').val('');
 		})
 
 		// displays total points accumulated by user
